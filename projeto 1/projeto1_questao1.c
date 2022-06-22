afa2e2f466ccd693c593f16390110a64e17992db
Chicolego <fj.2309@gmail.com>
2022-06-22 05:52:18 -0300

    v 0.3 Polimento
    - Removido gerador de número aleatório especial;
    - Removido variável "total_passos", já que "passos" pode cumprir sua função;
    + Implementação de rand() para a substituição do gerador especial;

--- projeto 1/projeto1_questao1.c
+++ projeto 1/projeto1_questao1.c
@@ -5,33 +5,6 @@
 
 #define SIZE 12
 
-/* Returns an integer in the range [0, n).
- *
- * Uses rand(), and so is affected-by/affects the same seed.
- */
-int randint(int n) {
-  if ((n - 1) == RAND_MAX) {
-    return rand();
-  } else {
-    // Supporting larger values for n would requires an even more
-    // elaborate implementation that combines multiple calls to rand()
-    assert (n <= RAND_MAX);
-
-    // Chop off all of the values that would cause skew...
-    int end = RAND_MAX / n; // truncate skew
-    assert (end > 0);
-    end *= n;
-
-    // ... and ignore results from rand() that fall above that limit.
-    // (Worst case the loop condition should succeed 50% of the time,
-    // so we can expect to bail out of this loop pretty quickly.)
-    int r;
-    while ((r = rand()) >= end);
-
-    return r % n;
-  }
-}
-
 //booleano que verifica se todos os vértices já foram visitados.
 int verificar(int vertices[]){
 	for(int i = 0; i < SIZE; i++){
@@ -39,7 +12,6 @@ int verificar(int vertices[]){
 			//printf("Vértice %d não foi acessado!\n", i);
 			return 0;
 		}
-	
 	}
 	return 1;
 }
@@ -55,19 +27,17 @@ void inicializar(int vertices[]){
 float gerar(int runs, int vertices[]){
 	int passos = 0;
 	int pos = 0;
-	int total_passos = 0;
 	
 	for(int i = 0; i < runs; i++){
 		
 		//inicialização da run.
-		passos = 0;
 		pos = 0;
 		inicializar(vertices);
 		vertices[pos]++;
 		
 		//processo de uma run.
 		while(!verificar(vertices)){
-			if(randint(2) == 1){
+			if((rand()%2) == 1){
 				pos = (((pos+1)%SIZE)+SIZE)%SIZE;
 				vertices[pos]++;			
 			}
@@ -78,11 +48,10 @@ float gerar(int runs, int vertices[]){
 			passos++;
 			//printf("Passo nº%d dado. \n", passos);
 		}
-		total_passos += passos;
 	}
 	
 	//retorno da quantidade média de passos para atingir o objetivo de uma run.
-	return ((float)total_passos/runs);
+	return ((float)passos/runs);
 }
 
 int main(int argc, char* argv[]){
@@ -92,9 +61,6 @@ int main(int argc, char* argv[]){
 	int runs = 0;
 	int vertices[SIZE];
 	srand(time(NULL));
-	
-	//inicialização do vetor de vértices. O conteúdo de cada índice representa a quantidade de acessos que já ocorreram ao respectivo vértice.
-	inicializar(vertices);
 
 	//leitura e avaliação dos parâmetros de entrada.
 	if(argc<2) {
